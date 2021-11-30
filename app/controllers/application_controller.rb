ebab535b567c2dde3ec3ca1b129ea798ce0891bc
Hazuki083 <ha882ki@gmail.com>
2021-11-30 03:03:03 +0000

    layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,9 @@ class ApplicationController < ActionController::Base
 
 
 def after_sign_in_path_for(resource)
-      if current_user
+      current_user
         flash[:notice] = "ログインに成功しました。今日の記録を入力しましょう!"
         blogs_path  #　指定したいパス
-      # else
-      #   flash[:notice] = "新規登録完了しました。次に年齢・性別・身長を入力してください"
-      #   edit_user_path(user.id)  #　指定したいパス
-      end
 end
 
 def after_sign_out_path_for(resource)
